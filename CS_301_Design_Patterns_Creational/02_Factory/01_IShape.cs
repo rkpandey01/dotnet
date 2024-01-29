50da9eb4bc587e83eef9841c2587f4876631b648
rkpandey01 <157519362+rkpandey01@users.noreply.github.com>
2024-01-30 00:03:10 +0530

    Added 02_Factory

---
+++ CS_301_Design_Patterns_Creational/02_Factory/01_IShape.cs
@@ -0,0 +1,7 @@
+﻿namespace _02_Factory
+{
+    internal interface IShape
+    {
+        void Draw();
+    }
+}
---
+++ CS_301_Design_Patterns_Creational/02_Factory/02_SubClasses.cs
@@ -0,0 +1,18 @@
+﻿namespace _02_Factory
+{
+    public class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing Circle");
+        }
+    }
+
+    public class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing Rectangle");
+        }
+    }
+}
