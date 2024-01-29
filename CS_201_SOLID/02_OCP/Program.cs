db02d880fdd6b8385ab176166f75c8656df4d717
rkpandey01 <157519362+rkpandey01@users.noreply.github.com>
2024-01-29 20:44:10 +0530

    Added details

--- CS_201_SOLID/02_OCP/Program.cs
+++ CS_201_SOLID/02_OCP/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Open / Closed Principle(OCP)");
+
+//Software entities(classes, modules, functions, etc.) should be open for extension 
+//    but closed for modification.
+
+//New functionality should be added by extending existing code rather than changing it.
+
+
+// Not following OCP
+/*
+public class Rectangle
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+}
+
+public class AreaCalculator
+{
+    public double CalculateArea(Rectangle rectangle)
+    {
+        return rectangle.Width * rectangle.Height;
+    }
+}
+*/
+
+
+// Following OCP
+public abstract class Shape
+{
+    public abstract double CalculateArea();
+}
+
+public class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
--- CS_201_SOLID/05_DIP/Program.cs
+++ CS_201_SOLID/05_DIP/Program.cs
@@ -1,2 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Dependency Inversion Principle (DIP)");
+
+
+/*
+ * High-level modules should not depend on low-level modules; both should depend on abstractions.
+ * Abstractions should not depend on details; details should depend on abstractions.
+ * Example: Instead of a high-level module directly depending on a low-level module,
+both should depend on an abstraction. 
+For instance, if you have a class OrderProcessor that needs to interact with a database, 
+create an interface like Database and have both OrderProcessor 
+and the actual database implementation depend on this abstraction.
+ 
+
+
+// Not following DIP
+public class LightBulb
+{
+    public void TurnOn()
+    {
+        // Turn on the light bulb
+    }
+}
+
+public class Switch
+{
+    private readonly LightBulb bulb;
+
+    public Switch()
+    {
+        this.bulb = new LightBulb();
+    }
+
+    public void Operate()
+    {
+        // Operate the light switch
+        bulb.TurnOn();
+    }
+}
+
+ 
+ */
+
+
+
+// Following DIP
+public interface ISwitchable
+{
+    void TurnOn();
+}
+
+public class LightBulb : ISwitchable
+{
+    public void TurnOn()
+    {
+        // Turn on the light bulb
+    }
+}
+
+public class Switch
+{
+    private readonly ISwitchable device;
+
+    public Switch(ISwitchable device)
+    {
+        this.device = device;
+    }
+
+    public void Operate()
+    {
+        // Operate the switch
+        device.TurnOn();
+    }
+}
