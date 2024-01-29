fb6ae2a667b5c99960c2322e51e3142c9852dae4
rkpandey01 <157519362+rkpandey01@users.noreply.github.com>
2024-01-29 23:33:05 +0530

    Lazy

--- CS_301_Design_Patterns_Creational/01_Singleton/04_LazyInitialization.cs
+++ CS_301_Design_Patterns_Creational/01_Singleton/04_LazyInitialization.cs
@@ -14,4 +14,12 @@ namespace _01_Singleton
 
         public static LazySingleton Instance => lazyInstance.Value;
     }
+
+    /*
+     * Here's what happens step by step:
+     * When the Lazy<MyClass> object is created, it doesn't immediately initialize the MyClass instance.
+     * When you access the Value property of the Lazy<MyClass> object, the MyClass instance is created and initialized for the first time.
+     * Subsequent accesses to the Value property will reuse the already initialized MyClass instance.
+
+     * */
 }
