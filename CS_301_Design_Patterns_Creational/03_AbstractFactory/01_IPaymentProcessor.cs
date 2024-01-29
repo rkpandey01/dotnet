11cb7d6176b7c211369bb75f3465e36eb02ffe75
rkpandey01 <157519362+rkpandey01@users.noreply.github.com>
2024-01-30 00:33:53 +0530

    Added 03_AbstractFactory

---
+++ CS_301_Design_Patterns_Creational/03_AbstractFactory/01_IPaymentProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_AbstractFactory
+{
+    // Abstract Product: PaymentProcessor
+    public interface IPaymentProcessor
+    {
+        void ProcessPayment();
+    }
+}
---
+++ CS_301_Design_Patterns_Creational/03_AbstractFactory/05_Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_AbstractFactory
+{
+    // Payment Client
+    public class PaymentClient
+    {
+        private IPaymentProcessor paymentProcessor;
+
+        public PaymentClient(IPaymentProcessorFactory factory)
+        {
+            paymentProcessor = factory.CreatePaymentProcessor();
+        }
+
+        public void MakePayment()
+        {
+            paymentProcessor.ProcessPayment();
+        }
+    }
+}
