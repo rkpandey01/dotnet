70791f19e19326c15a2ce6e525e8eae2353c710a
rkpandey01 <157519362+rkpandey01@users.noreply.github.com>
2024-01-30 21:48:01 +0530

    Added Builder pattern

---
+++ CS_301_Design_Patterns_Creational/04_Builder/02_IEmailBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Builder
+{
+    public interface IEmailBuilder
+    {
+        IEmailBuilder SetSender(string sender);
+        IEmailBuilder SetRecipient(string recipient);
+        IEmailBuilder SetSubject(string subject);
+        IEmailBuilder SetBody(string body);
+        Email Build();
+    }
+
+}
