0e2e04411ccc87b91edc67992a2e962bb80a65c2
rkpandey01 <157519362+rkpandey01@users.noreply.github.com>
2024-02-02 01:10:29 +0530

    Added Template

---
+++ CS_303_Design_Patterns_Behavioral/10_Template/1_01_TemplateClass.cs
@@ -0,0 +1,20 @@
+﻿namespace _10_Template
+{
+    abstract class BeverageTemplate
+    {
+        // Template method
+        public void MakeBeverage()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            AddCondiments();
+        }
+
+        // Abstract methods to be implemented by subclasses
+        protected abstract void BoilWater();
+        protected abstract void Brew();
+        protected abstract void PourInCup();
+        protected abstract void AddCondiments();
+    }
+}
