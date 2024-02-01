ce161280f5c659394dd0333d8a8c4f6c8f9789d7
rkpandey01 <157519362+rkpandey01@users.noreply.github.com>
2024-02-02 00:35:27 +0530

    Added State

---
+++ CS_303_Design_Patterns_Behavioral/05_State/1_01_IState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_State
+{
+    public interface IAudioPlayerState
+    {
+        void Play(AudioPlayer context);
+        void Pause(AudioPlayer context);
+        void Stop(AudioPlayer context);
+    }
+
+}
---
+++ CS_303_Design_Patterns_Behavioral/05_State/1_03_Context.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_State
+{
+    public class AudioPlayer
+    {
+        private IAudioPlayerState currentState;
+
+        public AudioPlayer()
+        {
+            // Initialize the player with the Stopped state
+            currentState = new StoppedState();
+        }
+
+        public void ChangeState(IAudioPlayerState newState)
+        {
+            currentState = newState;
+        }
+
+        public void Play()
+        {
+            currentState.Play(this);
+        }
+
+        public void Pause()
+        {
+            currentState.Pause(this);
+        }
+
+        public void Stop()
+        {
+            currentState.Stop(this);
+        }
+    }
+
+}
